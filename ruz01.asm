78514be237189a302bae7b4a7bc5cc747ae47d1a
gordian-eggo <columbus.knot@gmail.com>
2018-09-25 02:34:58 +0800

    Finished exer 01, it is buggy but it's something

--- ruz01.asm
+++ ruz01.asm
@@ -1,26 +1,28 @@
-; RUZ, Julianne Marie
-; 2014-04280
-; CMSC 131 AB-2L
-; Exer 01: Get the computed average of two 3-digit inputs
-
 section .data
 	prompt db "Enter a 3-digit number: "
 	prompt_len equ $-prompt
+	show_ave db "Average: "
+	sa_len equ $-show_ave
+	show_rem db " rem "
+	sr_len equ $-show_rem
 	newline db 10
 
 section .bss
 	f_huns resb 1
 	f_tens resb 1
 	f_ones resb 1
+	num1 resw 1
 	s_huns resb 1
 	s_tens resb 1
 	s_ones resb 1
-	num1 resw 1
 	num2 resw 1
-	ave_hun resb 1
+	ave_huns resb 1
 	ave_tens resb 1
 	ave_ones resb 1
 	quotient resb 1
+	rem_huns resb 1
+	rem_tens resb 1
+	rem_ones resb 1
 	remainder resb 1
 	test_value resb 1
 
@@ -37,17 +39,16 @@ _start:
 	mov edx, prompt_len
 		int 80h
 
-	; get tens and ones digit of first input
-	; then convert to integers
-
-	mov eax, 3
-	mov ebx, 0
+	; get first num 
+	
+	mov eax, 3 
+	mov ebx, 0 
 	mov ecx, f_huns
 	mov edx, 1
 		int 80h
 
 	sub byte [f_huns], 30h
-
+	
 	mov eax, 3
 	mov ebx, 0
 	mov ecx, f_tens
@@ -55,78 +56,186 @@ _start:
 		int 80h
 
 	sub byte [f_tens], 30h
-
+	
 	mov eax, 3
 	mov ebx, 0
 	mov ecx, f_ones
-	mov edx, 3
+	mov edx, 2
 		int 80h
-	
-	sub byte [f_ones], 30h
 
-	; prompt user for second number and convert to integer
+	sub byte [f_ones], 30h
+	
+	; prompt user for second 3-digit number
 
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, prompt
 	mov edx, prompt_len
-		int 80h
-
-	mov eax, 3
-	mov ebx, 0
+	int 80h
+	
+	; get second num 
+	
+	mov eax, 3 
+	mov ebx, 0 
 	mov ecx, s_huns
 	mov edx, 1
 		int 80h
-
+	
 	sub byte [s_huns], 30h
 
 	mov eax, 3
 	mov ebx, 0
 	mov ecx, s_tens
 	mov edx, 1
+		int 80h
 
 	sub byte [s_tens], 30h
-
+	
 	mov eax, 3
 	mov ebx, 0
 	mov ecx, s_ones
-	mov edx, 3
+	mov edx, 2
 		int 80h
-	
-	sub byte [s_ones], 30h
+    
+    sub byte [s_ones], 30h
+    
+    ; convert num1 entries to integer
+ 
+    mov al, [f_huns]
+    mov bl, 100
+    mul byte bl 
+    mov [num1], ax
+   
+    mov al, [f_tens]
+    mov bl, 10
+    mul byte bl
+    
+    add byte[num1], al
+    mov al, [f_ones]
+    add byte[num1], al 
+    
+    ; convert num2 entries to integer
+    mov al, [s_huns]
+    mov bl, 100
+    mul byte bl 
+    mov word[num2], ax
+    
+    mov al, [s_tens]
+    mov bl, 10
+    mul byte bl
+    
+    add byte[num2], al
+    mov al, [s_ones]
+    add byte[num2], al 
+    
+    ; add both numbers
+    mov ax, [num2]
+    add [num1], ax
+    
+    ; divide by 2 to get average
+    mov ax, [num1]
+    mov bl, 2
+    div byte bl
+    
+    mov [quotient], al
+    mov [remainder], ah
+    
+    ; convert quotient to characters
+    ; and move to the average number's digit variables
+
+    mov ah, 0							; makes sure that the value in ah is 0 before dividing
+
+    mov al, [quotient]
+    mov bl, 100
+    div byte bl
+    
+    mov [ave_huns], al
+    mov [ave_tens], ah
+    
+    mov ah, 0
+    mov al, [ave_tens]
+    mov bl, 10
+    div byte bl
+    
+    mov [ave_tens], al
+    mov [ave_ones], ah
+    
+    add byte[ave_huns], 30h
+    add byte[ave_tens], 30h
+    add byte[ave_ones], 30h
+
+    ; do the same thing for the remainder
+
+    mov ah, 0						; makes sure that the value in ah is 0 before dividing
+
+    mov al, [remainder]
+    mov bl, 100
+    div byte bl
+    
+    mov [rem_huns], al
+    mov [rem_tens], ah
+    
+    mov ah, 0
+    mov al, [rem_tens]
+    mov bl, 10
+    div byte bl
+    
+    mov [rem_tens], al
+    mov [rem_ones], ah
+    
+    add byte[rem_huns], 30h
+    add byte[rem_tens], 30h
+    add byte[rem_ones], 30h
+    
+    ; print the results
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, show_ave
+    mov edx, sa_len
+    	int 80h
 
 	mov eax, 4
 	mov ebx, 1
-	mov ecx, newline
+	mov ecx, ave_huns
+	mov edx, 1
+		int 80h
+	
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, ave_tens
+	mov edx, 1
+		int 80h
+	
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, ave_ones
 	mov edx, 1
 		int 80h
 
-	; convert the digits into one whole number
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, show_rem
+	mov edx, sr_len
+		int 80h
 
-	mov al, [f_huns]
-	mov bl, 100
-	mul byte bl
-	mov [f_huns], al 	; value of f_huns is now the hundreds digit * 100
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, rem_huns
+	mov edx, 1
+		int 80h
 
-	mov al, [f_tens]
-	mov bl, 10
-	mul byte bl 
-	mov [f_tens], al 	; value of f_tens is now tens digit * 10
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, rem_tens
+	mov edx, 1
+		int 80h		
 
-	mov al, [f_huns]
-	add al, [f_tens]
-	add al, [f_ones]
-	mov [num1], al
-
-	mov al, [num1]
-	mov bl, 100
-	div byte bl
-	mov [f_huns], al
-
-	mov [f_tens], ah
-	mov al, [f_tens]
-	mov bl, 10
-	div word bl
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, rem_ones
+	mov edx, 1
+		int 80h
 
 	mov eax, 4
 	mov ebx, 1
@@ -139,3 +248,6 @@ _start:
 	mov eax, 1
 	mov ebx, 0
 		int 80h
+
+; ERRORS:
+; 	> only works properly up to input value 549. 'di ko na makeri-keri
