3486d7465e18ef471c69aedd3e077958b5f905eb
gordian-eggo <columbus.knot@gmail.com>
2018-09-30 12:18:46 +0800

    Loop works great, but is printing unwanted inputs

--- ruz04.asm
+++ ruz04.asm
@@ -1,3 +1,9 @@
+; RUZ, Julianne Marie
+; 2014-04280
+; CMSC 131 AB-2L
+; Exercise 04: Take two 2-digit inputs N and M and output all numbers from 1 to N
+;			   that are divisible by M
+
 section .data
 	n_input db "Enter value for N: "
 	nlen equ $-n_input
@@ -9,6 +15,8 @@ section .data
 	dblen equ $-div_by
 	colon db ": "
 	clen equ $-colon
+	space_char db " "
+	sclen equ $-space_char
 	test_text db "hi"
 	tlen equ $-test_text
 	newline db 10
@@ -23,10 +31,12 @@ section .bss
 	m_val resb 1
 	print_n resb 1
 	print_m resb 1
+	iterator resb 1
+	remainder resb 1
 	d_ten resb 1
 	d_one resb 1
-	divisible resb 1
 	temp resb 1
+	divisible resb 1
 
 section .text
 
@@ -46,6 +56,7 @@ _start:
 	mov ebx, 0
 	mov ecx, n_ten
 	mov edx, 1
+
 		int 80h
 
 	sub byte [n_ten], 30h
@@ -62,7 +73,7 @@ _start:
 	mov bl, 10
 	mul byte bl
 	add byte al, [n_one]
-	mov [n_val], al
+	mov byte[n_val], al
 
 	mov eax, 4
 	mov ebx, 1
@@ -90,99 +101,80 @@ _start:
 	mov bl, 10
 	mul byte bl
 	add byte al, [m_one]
-	mov [m_val], al
+	mov byte[m_val], al
 
-	mov al, [n_val]							; copy values int print_n and print_m for printing in the 
-	mov [print_n], al 						; output sentence
+	mov byte[iterator], 0
+	mov byte[divisible], 1						; go through N numbers starting from 1
+	mov byte[temp], 1
 
-	mov al, [m_val]
-	mov [print_m], al
-	mov byte[temp], 0
-	mov ecx, 0
+find_and_print_divisible:						; find and print the numbers that are divisible by M
 
-	mov ecx, 3
-
-	; print the output sentence
-
-find_and_print_divisible:
-
-	mov [temp], ecx
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, test_text
-	mov edx, tlen
-		int 80h
-
-	mov ecx, [temp]
-	dec ecx
-;	cmp ecx, 0
-;	je test_print
-	loop find_and_print_divisible
-
-; test print values
-
-test_print:
-
-	mov al, [n_val]
-	mov bl, 10
+	mov al, [divisible]
+	mov bl, [m_val]
 	div byte bl
-	mov [n_ten], al
-	mov [n_one], ah
 
-	add byte[n_ten], 30h
-	add byte[n_one], 30h
+	mov [remainder], ah
+	
+	cmp ah, 0
+	je print_number
 
-	mov al, 0
-	mov ah, 0
+	inc byte[iterator]
+	inc byte[divisible]
+	inc byte[temp]
 
-	mov al, [m_val]
-	mov bl, 10
-	div byte bl
-	mov [m_ten], al
-	mov [m_one], ah
-
-	add byte[m_ten], 30h
-	add byte[m_one], 30h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, n_ten
-	mov edx, 1
-		int 80h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, n_one
-	mov edx, 1
-		int 80h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, newline
-	mov edx, 1
-		int 80h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, m_ten
-	mov edx, 1
-		int 80h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, m_one
-	mov edx, 1
-		int 80h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, newline
-	mov edx, 1
-		int 80h
+	mov al, [iterator]
+	cmp al, [n_val]
+	je exit
+	jmp find_and_print_divisible
 
 exit:
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, newline
+	mov edx, 1
+		int 80h
 
 	mov eax, 1
 	mov ebx, 0
 		int 80h
+
+; test print values
+
+print_number:
+
+	mov al, [temp]
+	mov bl, 10
+	div byte bl
+	mov [d_ten], al
+	mov [d_one], ah
+
+	add byte[d_ten], 30h
+	add byte[d_one], 30h
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, d_ten
+	mov edx, 1
+		int 80h
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, d_one
+	mov edx, 1
+		int 80h
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, space_char
+	mov edx, 1
+		int 80h
+
+	inc byte[iterator]
+	inc byte[divisible]
+	inc byte[temp]
+
+	mov al, [n_val]
+	cmp byte[iterator], al
+	jne find_and_print_divisible
+	jmp exit
