f3630f60badc0d8a85476e621eb5a3dbb6a127c2
gordian-eggo <columbus.knot@gmail.com>
2018-10-27 11:39:25 +0800

    Reworked code, but code is still so very spaghetti Eminem's puking it up on his sweater.

--- ruz05.asm
+++ ruz05.asm
@@ -2,7 +2,7 @@
 ; 2014-04280
 ; CMSC 131 AB-2L
 ; Exer 05: Print prime numbers from 1 to N where N is a two-digit number.
-;		   Global variables may not be used inside labels accessing the stack.	
+;		   Global variables may not be used inside the stack.	
 
 section .data
 
@@ -10,9 +10,18 @@ section .data
 	plen equ $-prompt
 	result db "Prime numbers: "
 	rlen equ $-result
-	space db " "
+	space db " space "
 	slen equ $-space
+	test db "here"
+	tlen equ $-test
 	newline db 10
+
+section .bss
+	tens resw 1
+	ones resw 1
+	num resw 1
+	factors resw 1
+	counter resw 1
 
 section .text
 
@@ -20,10 +29,20 @@ section .text
 
 _start: 
 
-	sub esp, 2
+	push tens			;	[ebp + 12] on the stack
+	push ones			;	[ebp + 10] on the stack
+	push num 			; 	[ebp + 8] on the stack
+	push factors 		;	[ebp + 6] on the stack
+	push counter 		;	[ebp + 4] on the stack
 	call get_num
 
 exit:
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, newline
+	mov edx, 1
+		int 80h
 
 	mov eax, 1
 	mov ebx, 0
@@ -32,7 +51,10 @@ exit:
 get_num:
 
 	mov ebp, esp			; put PC on the stack
-	sub esp, 2				; allocate local variable
+
+	mov [ebp + 4], 0			; initializes counter and factors as 0 for later
+	mov [ebp + 6], 0			
+
 	
 	mov eax, 4
 	mov ebx, 1
@@ -42,71 +64,92 @@ get_num:
 
 	mov eax, 3					; get first digit
 	mov ebx, 0
-	lea ecx, [ebp + 4]
+	lea ecx, [ebp + 12]
 	mov edx, 1
 		int 80h
 
-	sub word[ebp + 4], 30h
+	sub word[ebp + 12], 30h
 
 	mov ah, 0					; convert the tens digit right away
-	mov ax, [ebp + 4]
-	mov bx, 10
+	mov ax, [ebp + 12]
+	mov bx, 12
 	mul word bx
-	mov [ebp - 2], ax			; move to [ebp - 2] on top of PC
+	mov [ebp + 8], ax			; move to [ebp + 8]
 
 	mov eax, 3					; get second digit
 	mov ebx, 0
-	lea ecx, [ebp + 4]
+	lea ecx, [ebp + 10]
 	mov edx, 2
 		int 80h
 
-	sub word[ebp + 4], 30h
+	sub word[ebp + 10], 30h
 
 	mov ah, 0
-	mov ax, [ebp + 4]
-	add word[ebp - 2], ax		; add ones digit to tens digit
-	mov cx, [ebp - 2]			; move the value into ax
+	mov ax, [ebp + 10]
+	add word[ebp + 8], ax		; add ones digit to tens digit. now num at [ebp + 8] = input
+	mov ax, [ebp + 8]			; move the value of [ebp + 8] to ax for comparison
 
-	add esp, 2
+	jmp print_primes
 
-	jmp print_num
+print_primes:
 
-print_num:
+check_if_prime:
 
-	sub esp, 4
+	sub esp, 4				; to store the possible prime number
+	mov [ebp - 2], 1			; sets the prime value as 1 for later
+	jmp print_test
 
-	mov [ebp - 2], cx			; move the value into the local value so you can clear the register
+	find_factors:
 
-	mov al, 0					; made extra sure to clear the register
-	mov ah, 0					
+		cmp word[ebp - 2], ax		; compare the number being tested to the value in ax
+		je exit						; if equal, exit
+		mov [ebp - 4], ax			; move the value in ax here so we can use it for dividing
+		inc word[ebp + 4]			; increment the counter
 
-	mov ax, [ebp - 2]			;  move the value back into ax from the local variable
-	mov bx, 10
-	div word bx
-	mov [ebp - 2], al 			; move tens to [ebp - 2]
-	mov [ebp - 4], ah			; move ones to [ebp - 4]
+		mov ah, 0
+		mov ax, [ebp + 4]			; check if [ebp + 4] is divisible by the value in ax
+		div word[ebp - 2], ax
+		cmp dx, 0
+		je inc_factor
 
-	add word[ebp - 2], 30h
-	add word[ebp - 4], 30h
+		; inc word[ebp - 2]
+		jmp find_factors
+
+			inc_factor:				; increment factor ([ebp + 6] on the stack)
+
+
+				inc word[ebp + 6]
+				cmp word[ebp + 6], 3
+				jge inc_prime_check		; if the number has 3 or more factors, increment the number you're prime-checking
+
+				mov bx, word[ebp + 4]
+				cmp word[ebp - 2], bx
+				je inc_checker
+				
+
+				jmp reset
+
+					inc_checker:
+
+						inc word[ebp - 2]		; increment the prime-checker then jmp back to the loop
+						jmp find_factors
+
+
+					reset:
+
+						mov word[ebp + 4], 0		; reset counter and factors to 0		
+						mov word[ebp + 6], 0
+						mov ax, word[ebp - 4]
+						mov bx, 0
+						inc word[ebp - 2]
+						jmp find_factors
+
+print_test:
 
 	mov eax, 4
 	mov ebx, 1
-	lea ecx, [ebp - 2]
-	mov edx, 1
+	mov ecx, test
+	mov edx, tlen
 		int 80h
 
-	mov eax, 4
-	mov ebx, 1
-	lea ecx, [ebp - 4]
-	mov edx, 1
-		int 80h
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, newline
-	mov edx, 1
-		int 80h
-
-	add esp, 4
-
-	ret 2
+	jmp find_factors
